34c8014909364c8b33722875110aa1337da5703b
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-05-30 17:38:57 +0500

    App  ichida  urls.py  fayl  yaratildi.  hamda  views  fayl  orqali  pagelarni  boshqarishga  to'g'rilandi.    Prayket  ichida  Settings  faylga  HTML  fayllarga  ruxsat berildi.  Hamda  APP  qo'shib qo'yildi

--- Html_Django_Prayekt/Html_Django_Prayekt/settings.py
+++ Html_Django_Prayekt/Html_Django_Prayekt/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Home_App'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Html_Django_Prayekt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
