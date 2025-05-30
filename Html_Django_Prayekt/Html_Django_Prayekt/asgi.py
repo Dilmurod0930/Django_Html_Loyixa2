28c48befdfaaed32680446c71c709f156808f89c
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-05-30 17:25:18 +0500

    Prayekt  yaratildi,  Manage.py  ishga  tushdi  Run  berishga  tayor

---
+++ Html_Django_Prayekt/Html_Django_Prayekt/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Html_Django_Prayekt project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Html_Django_Prayekt.settings')
+
+application = get_asgi_application()
